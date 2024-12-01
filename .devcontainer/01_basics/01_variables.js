6ecc6833afd37733b0fc1ce08e303f2933f0d43c
gurjit-creater <gurjitanand.992@gmail.com>
2024-12-01 17:45:09 +0000

    config

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144550
+let accountEmail = "gurjit@google.com"
+var accountPassword = "gurjit.gs"
+accountCity = "Delhi"
+let accountState;
+
+
+// accountId = 2 // not allowed
+accountEmail ="hchc@g.com"
+accountPassword = "logintov"
+accountCity = "Jaipur"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "33abc"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
